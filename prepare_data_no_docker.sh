993c7be2f0bc114e06750d30831a996b2fd699a4
Eran Heres <eran.heres@gmail.com>
2018-01-02 18:19:50 +0200

    change local setup

--- prepare_data_no_docker.sh
+++ prepare_data_no_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ ! $# -eq 2 ]
+if [ ! $# -eq 1 ]
   then
     echo "No argument supplied - usage "
     echo "prepare_data.sh <folder> <docker_name>"
@@ -7,11 +7,8 @@ if [ ! $# -eq 2 ]
 fi
 
 if [ ! -d $1 ]; then
-    mkdir $1
     mkdir -p $1/processed
 fi
-cd $1
-pwd
 
 if [ ! -d images ]; then
     wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
@@ -31,13 +28,13 @@ fi
 
 # convert to tfrecord
 echo converting to TFRecords...
-if [ ! -f processed/pet_train.record ]; then
-    python $2/object_detection/dataset_tools/create_pet_tf_record.py \
+if [ ! -f $1/processed/pet_train.record ]; then
+    python object_detection/dataset_tools/create_pet_tf_record.py \
             --label_map_path=object_detection/data/pet_label_map.pbtxt \
             --data_dir=. \
             --output_dir=.
-    cp pet_train_with_masks.record processed/pet_train.record
-    cp pet_val_with_masks.record processed/pet_val.record
+    cp pet_train_with_masks.record $1/processed/pet_train.record
+    cp pet_val_with_masks.record $1/processed/pet_val.record
 else
     echo TFRecords exists, skipping conversion
 fi
@@ -50,15 +47,15 @@ if [ ! -d faster_rcnn_resnet101_coco_11_06_2017 ]; then
 else
     echo premade model exists, skipping download
 fi
-cp faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.* processed/.
+cp faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.* $1/processed/.
 
-cp $2/object_detection/data/pet_label_map.pbtxt processed/.
-cp $2/object_detection/samples/configs/faster_rcnn_resnet101_pets.config ./pets.config
-cp $2/object_detection/samples/cloud/cloud.yml processed/.
+cp object_detection/data/pet_label_map.pbtxt $1/processed/.
+cp object_detection/samples/configs/faster_rcnn_resnet101_pets.config ./pets.config
+cp object_detection/samples/cloud/cloud.yml $1/processed/.
 
-cat pets.config | sed "s|PATH_TO_BE_CONFIGURED|`pwd`/processed|g" > pets_updated.config
+cat object_detection/samples/configs/faster_rcnn_resnet101_pets.config | sed "s|PATH_TO_BE_CONFIGURED|gs://com-tabtale-objectdetectiontest-data/data|g" > $1/processed/gcs_pets.config
 
 # getting object detection code and slim
-cp $2/dist/object_detection-0.1.tar.gz processed/.
-cp $2/slim/dist/slim-0.1.tar.gz processed/.
+cp $1/dist/object_detection-0.1.tar.gz processed/.
+cp $1/slim/dist/slim-0.1.tar.gz processed/.
 
